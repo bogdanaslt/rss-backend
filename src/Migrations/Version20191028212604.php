44937370c97615def5636d379a764b1585926c33
Bogdan <b.kustan@gmail.com>
2019-10-28 23:44:39 +0200

    User entity and registration enpoint

---
+++ src/Migrations/Version20191028212604.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191028212604 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('ALTER TABLE user ADD COLUMN created_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, email, password FROM user');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL)');
+        $this->addSql('INSERT INTO user (id, email, password) SELECT id, email, password FROM __temp__user');
+        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+}
---
+++ src/Service/UserService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserService
+{
+
+    /**
+     *
+     * @var PasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * 
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     *
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * 
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param ValidatorInterface $validator
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(
+            UserPasswordEncoderInterface $passwordEncoder,
+            ValidatorInterface $validator,
+            EntityManagerInterface $entityManager
+    )
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->validator = $validator;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * 
+     * @param User $user
+     * @return User
+     */
+    public function register(User $user)
+    {
+        $password = $this->passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        return $user;
+    }
+
+    /**
+     * 
+     * @param User $user
+     * @param type $groups
+     * @return type
+     */
+    public function validate(User $user, $groups = null)
+    {
+        $violations = $this->validator->validate($user, null, $groups);
+        $errors = [];
+        if ($violations->count() > 0) {
+            foreach ($violations as $violation) {
+                $errors[$violation->getPropertyPath()] = $violation->getMessage();
+            }
+        }
+
+        return $errors;
+    }
+
+}
